762529835e96416d7f860db56cb85dae6ed4d282
Michael Aubrey <mcaubrey511@gmail.com>
2016-05-13 05:50:29 +0000

    added some meta data, license, etc

--- userscript.js
+++ userscript.js
@@ -1,11 +1,42 @@
+/**
+  The MIT License (MIT)
+
+  Copyright (c) 2014 Jeppe Rune Mortensen
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of
+  this software and associated documentation files (the "Software"), to deal in
+  the Software without restriction, including without limitation the rights to
+  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+  the Software, and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**/
 // ==UserScript==
+// @id           RedditRetractableSidebar
 // @name         Reddit Retractable Sidebar
 // @namespace    http://michaelcharl.es
-// @version      0.2.5
+// @version      0.2.6
 // @description  Make Reddit's Sidebar Retractable
+// @icon         https://mca62511.github.io/reddit-retractable-sidebar/favicon.png
+// @icon64       https://mca62511.github.io/reddit-retractable-sidebar/favicon.png
 // @author       Michael Aubrey
+// @domain       reddit.com
+// @domain       www.reddit.com
 // @match        https://www.reddit.com/*
+// @match        https://reddit.com/*
+// @match        http://www.reddit.com/*
+// @match        http://reddit.com/*
 // @grant        none
+// @license         MIT
 
 // ==/UserScript==
 
