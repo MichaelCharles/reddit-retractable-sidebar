6c76c8cdfbba65e6513d867bd3d6acf4ad9817e1
Michael Aubrey <mcaubrey511@gmail.com>
2016-05-13 09:06:06 +0000

    Made responsive for mobile

--- script.js
+++ script.js
@@ -1,71 +1,74 @@
-      (function() {
-          'use strict';
-          var pseudoCookie = "false";
+/* global $ */
 
-          var leftIcon = '<i class="material-icons">chevron_left</i>';
-          var rightIcon = '<i class="material-icons">chevron_right</i>';
-          $("head").append("<link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet' type='text/css'>");
-          var $content = $("main");
-          var $toggleButton = $("<div></div>");
-          $toggleButton.css({
-              background: "#CEE3F8",
-              color: "#212121",
-              display: "flex",
-              'justify-content': "center",
-              'align-items': "center",
-              height: "42px",
-              width: "42px",
-              "font-size": "32px",
-              "border-radius": "100px",
-              "box-shadow": "0px 3px 5px 0px rgba(33,33,33,0.3)",
-              position: "fixed",
-              top: "50%",
-              right: "-7px",
-              cursor: "pointer"
-          });
-          var $sidebar = $("div.side");
+(function() {
+    'use strict';
+    var pseudoCookie = "false";
 
-          function hideSidebar() {
-              $sidebar.hide();
-              $content.css({
-                  "margin-right": "15px"
-              });
-              $toggleButton.html(leftIcon);
-              setCookie("isRetracted", "true", 30);
-          }
+    var leftIcon = '<i class="material-icons">chevron_left</i>';
+    var rightIcon = '<i class="material-icons">chevron_right</i>';
+    $("head").append("<link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet' type='text/css'>");
+    var $content = $("main");
+    var $toggleButton = $("<div></div>");
+    $toggleButton.css({
+        background: "#CEE3F8",
+        color: "#212121",
+        display: "flex",
+        'justify-content': "center",
+        'align-items': "center",
+        height: "42px",
+        width: "42px",
+        "font-size": "32px",
+        "border-radius": "100px",
+        "box-shadow": "0px 3px 5px 0px rgba(33,33,33,0.3)",
+        position: "fixed",
+        top: "50%",
+        right: "-7px",
+        cursor: "pointer"
+    });
+    var $sidebar = $("div.side");
 
-          function showSidebar() {
-              $sidebar.show();
-              $content.css({
-                  "margin-right": "335px"
-              });
-              $toggleButton.html(rightIcon);
-              setCookie("isRetracted", "false", 30);
-          }
+    function hideSidebar() {
+        $sidebar.hide();
+        $content.css({
+            "margin-right": "15px"
+        });
+        $toggleButton.html(leftIcon);
+        setCookie("isRetracted", "true", 30);
+    }
 
-          function setCookie(cname, cvalue, exdays) {
-              pseudoCookie = cvalue;
-          }
+    function showSidebar() {
+        $sidebar.show();
+        $content.css({
+            "margin-right": "335px"
+        });
+        $toggleButton.html(rightIcon);
+        setCookie("isRetracted", "false", 30);
+    }
 
-          function getCookie(cname) {
-              return pseudoCookie;
-          }
+    function setCookie(cname, cvalue, exdays) {
+        pseudoCookie = cvalue;
+    }
 
-          if (getCookie("isRetracted") == "true") {
-              hideSidebar();
-          }
-          else {
-              showSidebar();
-          }
-          $("body").append($toggleButton);
-          $toggleButton.click(function() {
-              var currentIcon = $toggleButton.html().trim();
-              if (currentIcon === rightIcon) {
-                  hideSidebar();
-              }
-              else {
-                  showSidebar();
-              }
-          });
+    function getCookie(cname) {
+        return pseudoCookie;
+    }
 
-      })();
+    if (getCookie("isRetracted") == "true") {
+        hideSidebar();
+    }
+    else {
+        showSidebar();
+    }
+    $("body").append($toggleButton);
+    $toggleButton.click(function() {
+        var currentIcon = $toggleButton.html().trim();
+        if (currentIcon === rightIcon) {
+            hideSidebar();
+        }
+        else {
+            showSidebar();
+        }
+    });
+
+
+})();
